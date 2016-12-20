bff4f7c428e700af1c379f0d3fbcfde453f09f4f
Narciso Nunez <narciso@soldeva.com>
2016-12-20 12:09:00 -0400

    Add Liquidacion Doctype fields and basic js functionality to some field

--- heladom/heladom/doctype/liquidacion/liquidacion.js
+++ heladom/heladom/doctype/liquidacion/liquidacion.js
@@ -4,5 +4,40 @@
 frappe.ui.form.on('Liquidacion', {
 	refresh: function(frm) {
 
+	},
+	international_order: function(frm){
+		frappe.call({
+			"method": "heladom.heladom.doctype.liquidacion.liquidacion.get_international_order_info",
+			args: {
+				"international_order": frm.doc.international_order
+			},
+			callback: function(data){
+				var skus = data.message;
+				skus.forEach(function(sku){
+					var row = frappe.model.add_child(frm.doc, "skus");
+					row.sku = sku.sku;
+					row.sku_name = sku.sku_name;
+					row.qty = sku.order_sku_total;
+					row.arancel = sku.arancel;
+
+					row.monto_arancel = row.qty * parseFloat(row.qty * row.arancel / 100).toFixed(2);
+				});
+
+				cur_frm.refresh();
+			}
+		});
 	}
+});
+frappe.ui.form.on('Liquidacion Items', {
+	arancel: function(frm, cdt, cdn){
+		var row = locals[cdt][cdn];
+		row.monto_arancel = row.qty * parseFloat(row.qty * row.arancel / 100).toFixed(2);
+		frm.refresh_field("skus");
+	},
+	total_cif_usd: function(frm, cdt, cdn){
+		var row = locals[cdt][cdn];
+		row.total_cif_dop = parseFloat(row.total_cif_usd * frm.doc.rate).toFixed(2);
+		frm.refresh_field("skus");
+	}
+
 });
--- heladom/heladom/doctype/liquidacion/liquidacion.py
+++ heladom/heladom/doctype/liquidacion/liquidacion.py
@@ -8,3 +8,13 @@ from frappe.model.document import Document
 
 class Liquidacion(Document):
 	pass
+
+@frappe.whitelist()
+def get_international_order_info(international_order):
+	result = frappe.db.sql("""SELECT ocp.sku, ocp.sku_name, ocp.order_sku_total, s.arancel
+								FROM `tabFactura Internacional` as fi
+								JOIN `tabOrden de Compra` as oc ON oc.name = fi.order
+								JOIN `tabOrden de Compra Productos` as ocp ON ocp.parent = oc.name
+								JOIN  `tabSKU` as s ON s.name = ocp.sku
+								WHERE fi.name = '%s'""" % (international_order,), as_dict=1)
+	return result
