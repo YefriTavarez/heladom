3cc34a4a0eaa73d5d67a951e4aa04abd1b2ffdef
Narciso Nunez <narciso@soldeva.com>
2016-12-20 01:08:21 -0400

    Update Factura internacional Doctype and add the basic functionality

--- heladom/heladom/doctype/factura_internacional/factura_internacional.js
+++ heladom/heladom/doctype/factura_internacional/factura_internacional.js
@@ -4,5 +4,28 @@
 frappe.ui.form.on('Factura Internacional', {
 	refresh: function(frm) {
 
+	},
+	order: function(frm){
+
+		frappe.call({
+			"method": "heladom.heladom.doctype.factura_internacional.factura_internacional.get_order_skus",
+			args: {
+				order: frm.doc.order
+			},
+			callback: function(data){
+				var skus = data.message;
+				skus.forEach(function(sku){
+
+					var row = frappe.model.add_child(frm.doc, "skus");
+					row.sku = sku.sku;
+					row.sku_name = sku.sku_name;
+					row.qty = sku.order_sku_total;
+					row.estimation_reference = sku.estimation_reference;
+				});
+
+				cur_frm.refresh();
+			}
+		});
+
 	}
 });
--- heladom/heladom/doctype/factura_internacional/factura_internacional.py
+++ heladom/heladom/doctype/factura_internacional/factura_internacional.py
@@ -7,4 +7,33 @@ import frappe
 from frappe.model.document import Document
 
 class FacturaInternacional(Document):
-	pass
+	def on_submit(self):
+		for sku in self.skus:
+			frappe.db.sql("""UPDATE `tabOrden de Compra Productos`
+							SET order_sku_total = %d
+							WHERE parent = '%s' AND sku = '%s'
+							""" % (sku.qty, self.order, sku.sku))
+
+			frappe.db.sql("""UPDATE `tabEstimacion SKUs`
+							SET order_sku_total = %d
+							WHERE parent = '%s' AND sku = '%s'
+							""" % (sku.qty ,sku.estimation_reference, sku.sku))
+
+		estimations = frappe.db.sql("""SELECT estimation 
+										FROM `tabEstimaciones Orden de Compra`
+										WHERE parent = '%s'""" % (self.order,), as_dict=1)
+
+		for estimation in estimations:
+			frappe.db.sql("""UPDATE `tabEstimacion de Compra`
+								SET was_received = 1
+								WHERE name = '%s' """ % (estimation.estimation,))
+
+
+@frappe.whitelist()
+def get_order_skus(order):
+	result = frappe.db.sql("""SELECT sku, sku_name, order_sku_total, estimation_reference
+								FROM `tabOrden de Compra Productos`
+								WHERE parent = '%s'
+							""" % (order,), as_dict=1)
+
+	return result
