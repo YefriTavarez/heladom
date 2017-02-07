00d29e869cd3cf3e076e66ef13f97a917a4b51c8
Yefri Tavarez <yefritavarez@hotmail.com>
2017-02-07 16:07:20 -0400

    Validate duplicates entries in Items table and Add focus request to Inventario Helado's Doctype when entering a valid item code.

--- heladom/heladom/doctype/inventario_helado/inventario_helado.js
+++ heladom/heladom/doctype/inventario_helado/inventario_helado.js
@@ -54,17 +54,40 @@ function get_sku_info(frm, skuCode){
 		callback: function(data){
 			var sku = data.message;
 			if (sku != "error") {
-				var row = frappe.model.add_child(frm.doc, "items");
+				var row = get_sku(frm.doc.items, skuCode);
+
+				if(! row){
+					row = frappe.model.add_child(frm.doc, "items");
+				}
+
 				row.sku = sku.name;
 				row.sku_name = sku.item;
 				row.sku_generic = sku.generic;
 				row.sku_generic_name = sku.generic_name;
 				cur_frm.refresh();
+				frappe.model.set_value(frm.doctype, frm.docname, "code", undefined);
+				var len = document.getElementsByClassName("close btn-open-row").length;
+				for(index = 0; index < len; index ++){
+					btn = document.getElementsByClassName("close btn-open-row")[index];
+					if((index+1) == len){
+						btn.click();
+					}
+				}
+				setTimeout(function(){ $("input[data-fieldname=sku_unces]").focus(); },500);
 			} else {
 				frappe.msgprint("No se ha encontrado ningun sku con este codigo.");
+				after(method=frappe.hide_msgprint, instant=false, time=4);
 			}
-			frappe.model.set_value(frm.doctype,
-                    frm.docname, "code", undefined);
 		}
 	});
+}
+
+function after(method, args=undefined, time=5){ if(!method){ return; } setTimeout(function(){ method(args);},time*1000); }
+
+function get_sku(array, sku){
+	for(index = 0; index < array.length; index ++){
+		if(array[index].sku == sku) return array[index];
+	}
+
+	return undefined;
 }
